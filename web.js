7f0abfa40ea824acb1e7d036d2ca4d157b1c0aa6
Dylan Barth <dylan.j.barth@gmail.com>
2013-07-10 01:32:24 +0000

    Ok now I think I got it

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   // response.send('Hello World 2!');
-    var outStr = fs.readFile('index.html', 'utf8');
+    var outStr = fs.readFileSync('index.html', 'utf8');
     response.send(outStr.toString());
 });
 
